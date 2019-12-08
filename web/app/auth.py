2712929f0072d391db8064139f10f4e4841d0707
MINH-PC\minhc <minhchau2236@gmail.com>
2019-12-09 00:09:45 +0700

    add auth logic

--- web/app/auth.py
+++ web/app/auth.py
@@ -6,7 +6,6 @@ auth = HTTPTokenAuth()
 
 @auth.verify_token
 def verify_token(token):
-    # first try to authenticate by token
     user = User.verify_auth_token(token)
     if not user:
       return False
--- web/app/routes.py
+++ web/app/routes.py
@@ -1,6 +1,8 @@
 from app import app, db
 from flask import Flask, abort, request, jsonify, g, url_for
 from app.models import User
+from flask_restful import Api, Resource
+from app.auth import auth
 
 @app.route('/')
 @app.route('/index')
@@ -14,10 +16,10 @@ def login():
     if username is None or password is None:
         abort(400) # missing arguments
     user = User.query.filter_by(username = username).first()
-    if not user or not user.verify_password(password):
+    if not user or not user.check_password(password):
        abort(401) # invalid user
-    token = g.user.generate_auth_token()
-    return jsonify({ 'user': user, 'token': token.decode('ascii') })
+    token = user.generate_auth_token()
+    return jsonify({ 'id': user.id, 'name': user.username, 'token': token.decode('ascii') }), 200
 
 @app.route('/api/user', methods = ['POST'])
 def new_user():
@@ -34,6 +36,7 @@ def new_user():
     return jsonify({ 'username': user.username, 'Location': url_for('new_user', id = user.id, _external = True)}), 200, {'Location': url_for('new_user', id = user.id, _external = True)}
 
 @app.route('/api/user', methods = ['GET'])
+@auth.login_required
 def get_user():
     id = request.args.get('id')
     user = User.query.filter_by(id = id).first()
